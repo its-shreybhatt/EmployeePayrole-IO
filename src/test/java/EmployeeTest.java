2532f8671105285f84d24e7559606717367e5350
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-28 20:35:25 +0530

    Added : Code to Create Watcher Thread to watch Directory

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,4 +1,5 @@
 import org.apache.commons.io.FileUtils;
+import org.example.Java8WatchService;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class EmployeeTest {
@@ -40,6 +42,14 @@ public class EmployeeTest {
 
         Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
         Files.newDirectoryStream(playPath).forEach(System.out::println);
-        Files.newDirectoryStream(playPath, path -> path.toFile().isFile()&&path.toString().startsWith("temp")).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() &&
+                path.toString().startsWith("temp")).forEach(System.out::println);
+    }
+
+    @Test
+    public void givenADirectory_WhenWatchedListsAllTheActivities()throws IOException {
+        Path dir= Paths.get(HOME+"/"+PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchService(dir).processEvents();
     }
 }
