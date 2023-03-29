e285494f4473d727e4005370fcda6c1bacb27b54
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-29 21:17:58 +0530

    Added : Code to Store Data in a File and Pass the Test

--- src/main/java/org/example/EmployePayroleService.java
+++ src/main/java/org/example/EmployePayroleService.java
@@ -1,30 +1,46 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class EmployePayroleService {
 
-    public static void main(String[] args) {
-        ArrayList<EmployeePayroleData> employeePayroleList = new ArrayList<>();
-        EmployePayroleService employePayroleService = new EmployePayroleService();
-        Scanner input = new Scanner(System.in);
-        employePayroleService.readEmployeeData(input, employeePayroleList);
-        employePayroleService.writeEmployeeData(employeePayroleList);
+
+    public enum IOService {CONSOLE_IO, FILE_IO}
+
+    private static List<EmployeePayroleData> employeePayrollList;
+
+    public EmployePayroleService() {}
+
+
+    public EmployePayroleService(List<EmployeePayroleData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+
     }
 
-    public void readEmployeeData(Scanner input, ArrayList<EmployeePayroleData> employeePayroleList) {
+    public static void main(String[] args) {
+        employeePayrollList = new ArrayList<>();
+        EmployePayroleService employePayroleService = new EmployePayroleService();
+        Scanner input = new Scanner(System.in);
+        employePayroleService.readEmployeePayrollData(input);
+        employePayroleService.writeEmployeePayrollData(IOService.CONSOLE_IO);
+    }
+
+    public void readEmployeePayrollData(Scanner input) {
         System.out.println("Enter Employee Id ");
         int id = input.nextInt();
         System.out.println("Enter Employee Name");
         String name = input.next();
         System.out.println("Enter Employee Salary");
         double salary = input.nextDouble();
-        employeePayroleList.add(new EmployeePayroleData(id, name, salary));
+        employeePayrollList.add(new EmployeePayroleData(id, name, salary));
     }
 
-    public void writeEmployeeData(ArrayList<EmployeePayroleData> employeePayroleList) {
-        System.out.println("Data is = " + employeePayroleList);
+    public void writeEmployeePayrollData(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("Writting to Console \n" + employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIO().writeData(employeePayrollList);
     }
-
 }
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,15 +1,19 @@
-import org.apache.commons.io.FileUtils;
+import org.example.EmployePayroleService;
+import org.example.EmployeePayroleData;
+import org.example.FileUtils;
 import org.example.Java8WatchService;
 import org.junit.Assert;
 import org.junit.Test;
-
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
+
+import java.util.Arrays;
 import java.util.stream.IntStream;
+
+import static org.example.EmployePayroleService.IOService.FILE_IO;
 
 public class EmployeeTest {
 
@@ -22,7 +26,7 @@ public class EmployeeTest {
         Assert.assertTrue(Files.exists(homePath));
 
         Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-        if (Files.exists(playPath)) FileUtils.deleteQuietly(playPath.toFile());
+        if (Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
         Assert.assertTrue(Files.notExists(playPath));
 
 
@@ -47,9 +51,19 @@ public class EmployeeTest {
     }
 
     @Test
-    public void givenADirectory_WhenWatchedListsAllTheActivities()throws IOException {
-        Path dir= Paths.get(HOME+"/"+PLAY_WITH_NIO);
+    public void givenADirectory_WhenWatchedListsAllTheActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
         Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
         new Java8WatchService(dir).processEvents();
+    }
+
+    @Test
+    public void given3Employees_WhenWritten_ShouldStoreInAFile() {
+        EmployeePayroleData[]arayOfEmps = {
+                new EmployeePayroleData(1, "Shrey", 25000.0),
+                new EmployeePayroleData(2, "Kartikeya", 28000.0),
+                new EmployeePayroleData(3, "Akshay", 27500.5)};
+        EmployePayroleService object = new EmployePayroleService(Arrays.asList(arayOfEmps));
+        object.writeEmployeePayrollData(FILE_IO);
     }
 }
